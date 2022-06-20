26dea84ddb538accb8f091799ffa6448303b68cb
kaiokins <66028140+kaiokins@users.noreply.github.com>
2022-06-20 21:18:42 +0200

    Usunięcie config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = "jvbndeocdfmnpc";
-const PASSWORD = "7fbd60af048b11c624bb0b6685a901a108f4077ae07fe5a08d64e668c63e4408";
-const HOST = "ec2-54-228-125-183.eu-west-1.compute.amazonaws.com";
-const DATABASE = "d160gsirbjp7hn";
